18892b4057242a6890f73e5eaca99debc01ea297
Leigh Carr <leighcarr@Leighs-MacBook-Pro.local>
2019-07-09 18:39:20 -0400

    because it has been a while

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-
+// figure out how to d/t autofocus
 //this is for the big modal aka leadModal
 $(document).ready(function() {
-$('#go-btn').click(function(){
-  var zipcode = $('#zipcode').val();
-  $('#inputzipcode').val(zipcode);
+
+  $('#go-btn').click(function(){
+    var zipcode = $('#zipcode').val();
+    $('#smallZip').val(zipcode);
   });
+
+  $('#submit').click(function(){
+    console.log("submitted");
+  });
+
+  $("#quote_submit").click(function(e){
+
+    e.preventDefault();
+    // formSubmit(e.currentTarget.name);
+    formSubmit('smallForm');
+  });
+
 });
 // console.log("ready!") --
 
-$('#inputEmail').click(function(){
-  console.log('#submit').val(email);
-});
+
+
+function formSubmit(name) {
+  var serializedData = $(document.forms[name]).serializeObject();
+  var url = 'https://script.google.com/macros/s/AKfycbywAdx0h4RMpnyS0bAms9iW7QFRDdBN4rrEMDQvlADDro8qZko/exec'
+
+  var jqxhr = $.ajax({
+    url,
+    method: "GET",
+    dataType: "json",
+    data: serializedData,
+    success: function(data){
+      console.log('success', data);
+    }
+  });
+}
+
+// $('#inputEmail').click(function(){
+//   console.log('#submit').val(email);
+// });
+
+
+//https://script.google.com/macros/s/AKfycbywAdx0h4RMpnyS0bAms9iW7QFRDdBN4rrEMDQvlADDro8qZko/exec
